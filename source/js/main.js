2a3a5b3cd3c5e1b42f43d4c93079c1a3ed9709b8
Aleksandra <sashkin_08@mail.ru>
2023-10-13 18:18:12 +0300

    naming

--- source/js/main.js
+++ source/js/main.js
@@ -8,33 +8,30 @@ window.ResizeObserver = ResizeObserver;
 document.addEventListener('DOMContentLoaded', () => {
 
   /*открытия-закрытия меню*/
-  const btn = document.querySelector('#nav-toggle'),
+  const menuBtn = document.querySelector('#nav-toggle'),
         menu = document.querySelector('nav');
 
-  btn.addEventListener('click', function (event) {
-      event.preventDefault();
-      menu.classList.toggle('open');
-      btn.classList.toggle('open');
+  menuBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    menu.classList.toggle('open');
+    menuBtn.classList.toggle('open');
   });
 
   /*выбор языка*/
-  const lang = document.querySelector('.lang__choice'),
+  const langList = document.querySelector('.lang__choice'),
         currentLangBtn = document.querySelector('.lang__active'),
-        currentLangValue = document.querySelector('.lang__value');
+        currentLangValue = currentLangBtn.querySelector('.lang__value');
 
   currentLangBtn.addEventListener('click', function (event) {
-    lang.classList.toggle('open');
+    langList.classList.toggle('open');
   });
-  lang.addEventListener('click', function (event) {
+  langList.addEventListener('click', function (event) {
     if (event.target.dataset.lang) {
       const langActive = event.target.dataset.lang;
-      lang.querySelector('.active').classList.remove('active');
+      langList.querySelector('.active').classList.remove('active');
       event.target.classList.add('active');
       currentLangValue.textContent = langActive;
     }
-    lang.classList.toggle('open');
+    langList.classList.toggle('open');
   });
 });
-
-
-
